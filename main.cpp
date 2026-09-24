e8b2a19cd865157fcecee45c6f088c2b526fd344
Ismael-schl <ihadi678@insite.4cd.edu>
2026-09-23 20:26:22 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 //Lab 15 | COMSC 210 | Ismael Hadi
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
+
+const int SIZE = 4;
 
 class Movie {
     private:
@@ -16,7 +19,7 @@ class Movie {
     string getWriter() {
         return sWriter;
     }
-    void setYearr(int y){
+    void setYear(int y){
         year = y;
     }
     int getYear() {
@@ -25,7 +28,30 @@ class Movie {
     void setTitle(string t){
         title= t;
     }
-    string getWriter() {
-        return sWriter;
+    string getTitle() {
+        return title;
+    }
+    void print() {
+        cout << "Movie: " << title << endl;
+        cout << "    Year released: " << year << endl;
+        cout << "    Screenwriter: " << sWriter << endl << endl;
     }
 };
+
+int main() {
+    ifstream fin("moviedata.txt");
+    Movie arr[SIZE];
+    Movie temp;
+    string title;
+    int year;
+    string writer;
+
+    for (int i = 0; i < SIZE; i++) {
+        getline(fin, title);
+        fin >> year;
+        fin.ignore();
+        getline(fin, writer);
+        
+    }
+
+}
